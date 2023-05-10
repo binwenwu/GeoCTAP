c7c71dae5277f3b9a5c3202b3317831da434cc76
Binwen Wu <2020302131043@whu.edu.cn>
2023-05-10 08:26:37 +0800

    新版

---
+++ sql/geoctap.sql
@@ -0,0 +1,334 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80031
+ Source Host           : localhost:3306
+ Source Schema         : geoctap
+
+ Target Server Type    : MySQL
+ Target Server Version : 80031
+ File Encoding         : 65001
+
+ Date: 06/05/2023 22:19:00
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for activity
+-- ----------------------------
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE `activity`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '活动名称',
+  `fund` int(0) NULL DEFAULT NULL COMMENT '活动资金',
+  `times` int(0) NULL DEFAULT NULL COMMENT '志愿时长',
+  `state` tinyint(1) NULL DEFAULT NULL COMMENT '是否开始',
+  `charge_id` int(0) NULL DEFAULT NULL COMMENT '负责人id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of activity
+-- ----------------------------
+INSERT INTO `activity` VALUES (1, '宝贝回家活动', 20, 80, 1, 2);
+INSERT INTO `activity` VALUES (2, '神兔侠活动', 10, 45, 1, 2);
+INSERT INTO `activity` VALUES (3, '团员系统活动', 10, 30, 1, 2);
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容',
+  `user` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '发布人',
+  `time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '发布时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article
+-- ----------------------------
+INSERT INTO `article` VALUES (1, '文章1', '## 项目简介\nGeoCTAP\n\n## 技术栈\n\n后端：\n\n- Springboot2\n- Hutool\n- Apache POI\n- Lombok\n- Mybatis/Mybatis-plus\n\n前端：\n\n- Vue2\n- VueX\n- Vue-Router\n- ElementUI\n- Axios\n\n\n\n\n测试：\n\n- Postman\n- Swagger\n\n\n\n## 功能清单\n\n- 登录注册\n- 基于RBAC的权限模型（可分配角色和菜单权限）\n- 单表增删改查（包括分页模糊查询）\n- 登录验证码\n- 文件上传\n- 导入导出\n- 服务器部署\n- 文本编辑器\n- 树形表格（数据可作为树形菜单使用）\n- JWT权限\n- 后端1对多查询\n- 批量删除\n- 地图服务\n- 在线留言\n- 个人头像\n- 修改密码\n- 整合Echarts图表\n\n\n\n\n\n\n\n\n\n', '贪肯奇', '2023-03-20 19:22:58');
+INSERT INTO `article` VALUES (2, '文章2', '内容2', '贪肯奇', '2023-05-22 19:22:58');
+
+-- ----------------------------
+-- Table structure for building
+-- ----------------------------
+DROP TABLE IF EXISTS `building`;
+CREATE TABLE `building`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '楼栋',
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '单元',
+  `pid` int(0) NULL DEFAULT NULL COMMENT '父级id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of building
+-- ----------------------------
+INSERT INTO `building` VALUES (1, '1栋', '1栋', NULL);
+INSERT INTO `building` VALUES (2, '2栋', '2栋', NULL);
+INSERT INTO `building` VALUES (3, '1单元', '1单元', 1);
+INSERT INTO `building` VALUES (4, '2单元', '2单元', 1);
+INSERT INTO `building` VALUES (5, '1单元', '1单元', 2);
+INSERT INTO `building` VALUES (6, '2单元', '2单元', 2);
+INSERT INTO `building` VALUES (7, '101', '101', 3);
+INSERT INTO `building` VALUES (8, '201', '201', 3);
+INSERT INTO `building` VALUES (9, '101', '101', 4);
+INSERT INTO `building` VALUES (10, '201', '201', 4);
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '课程名称',
+  `score` int(0) NULL DEFAULT NULL COMMENT '学分',
+  `times` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上课时间',
+  `state` tinyint(1) NULL DEFAULT NULL COMMENT '是否开课',
+  `teacher_id` int(0) NULL DEFAULT NULL COMMENT '授课老师id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES (1, '宝贝回家', 10, '40', NULL, 2);
+INSERT INTO `course` VALUES (2, '神兔侠', 10, '45', NULL, 2);
+INSERT INTO `course` VALUES (3, '团员系统', 10, '30', NULL, 2);
+
+-- ----------------------------
+-- Table structure for normal_activity
+-- ----------------------------
+DROP TABLE IF EXISTS `normal_activity`;
+CREATE TABLE `normal_activity`  (
+  `normal_id` int(0) NOT NULL,
+  `activity_id` int(0) NOT NULL,
+  PRIMARY KEY (`normal_id`, `activity_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of normal_activity
+-- ----------------------------
+INSERT INTO `normal_activity` VALUES (3, 1);
+INSERT INTO `normal_activity` VALUES (3, 2);
+INSERT INTO `normal_activity` VALUES (3, 3);
+
+-- ----------------------------
+-- Table structure for student_course
+-- ----------------------------
+DROP TABLE IF EXISTS `student_course`;
+CREATE TABLE `student_course`  (
+  `student_id` int(0) NOT NULL,
+  `course_id` int(0) NOT NULL,
+  PRIMARY KEY (`student_id`, `course_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student_course
+-- ----------------------------
+INSERT INTO `student_course` VALUES (1, 1);
+INSERT INTO `student_course` VALUES (28, 1);
+INSERT INTO `student_course` VALUES (28, 2);
+
+-- ----------------------------
+-- Table structure for sys_dict
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dict`;
+CREATE TABLE `sys_dict`  (
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '内容',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '类型'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_dict
+-- ----------------------------
+INSERT INTO `sys_dict` VALUES ('user', 'el-icon-user', 'icon');
+INSERT INTO `sys_dict` VALUES ('house', 'el-icon-house', 'icon');
+INSERT INTO `sys_dict` VALUES ('menu', 'el-icon-menu', 'icon');
+INSERT INTO `sys_dict` VALUES ('s-custom', 'el-icon-s-custom', 'icon');
+INSERT INTO `sys_dict` VALUES ('s-grid', 'el-icon-s-grid', 'icon');
+INSERT INTO `sys_dict` VALUES ('document', 'el-icon-document', 'icon');
+INSERT INTO `sys_dict` VALUES ('coffee', 'el-icon-coffee\r\n', 'icon');
+INSERT INTO `sys_dict` VALUES ('s-marketing', 'el-icon-s-marketing', 'icon');
+
+-- ----------------------------
+-- Table structure for sys_file
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_file`;
+CREATE TABLE `sys_file`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件类型',
+  `size` bigint(0) NULL DEFAULT NULL COMMENT '文件大小(kb)',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '下载链接',
+  `md5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '文件md5',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  `enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否禁用链接',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 84 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_file
+-- ----------------------------
+INSERT INTO `sys_file` VALUES (85, 'GitBash.png', 'png', 14, 'http://localhost:9090/file/4d69f050b38b49829d26e5f060dd22ac.png', '474b45dafe51b4a890470f89f031c065', 0, 1);
+INSERT INTO `sys_file` VALUES (86, 'test.png', 'png', 30, 'http://localhost:9090/file/3f7b7d1944a447c48f42d1a67cd82269.png', '8e82e171c126ab1aa508dd43c75db94e', 0, 1);
+INSERT INTO `sys_file` VALUES (87, 'GitBash.png', 'png', 14, 'http://localhost:9090/file/4d69f050b38b49829d26e5f060dd22ac.png', '474b45dafe51b4a890470f89f031c065', 0, 1);
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图标',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '描述',
+  `pid` int(0) NULL DEFAULT NULL COMMENT '父级id',
+  `page_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '页面路径',
+  `sort_num` int(0) NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 43 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES (1, '主页', '/home', 'el-icon-house', NULL, NULL, 'Home', 0);
+INSERT INTO `sys_menu` VALUES (2, '数据报表', '/dashbord', 'el-icon-s-marketing', '', NULL, 'Dashbord', 100);
+INSERT INTO `sys_menu` VALUES (3, '请作者喝杯咖啡', '/donate', 'el-icon-coffee\r\n', NULL, NULL, 'Donate', 200);
+INSERT INTO `sys_menu` VALUES (4, '系统管理', NULL, 'el-icon-s-grid', NULL, NULL, NULL, 300);
+INSERT INTO `sys_menu` VALUES (5, '志愿活动', '/activity', 'el-icon-menu', NULL, NULL, 'Activity', 201);
+INSERT INTO `sys_menu` VALUES (6, '文章管理', '/article', 'el-icon-menu', NULL, NULL, 'Article', 999);
+INSERT INTO `sys_menu` VALUES (7, '文章查询', '/timeSearch', 'el-icon-menu', NULL, NULL, 'TimeSearch', 999);
+INSERT INTO `sys_menu` VALUES (41, '用户管理', '/user', 'el-icon-user', NULL, 4, 'User', 301);
+INSERT INTO `sys_menu` VALUES (42, '角色管理', '/role', 'el-icon-s-custom', NULL, 4, 'Role', 302);
+INSERT INTO `sys_menu` VALUES (43, '菜单管理', '/menu', 'el-icon-menu', NULL, 4, 'Menu', 303);
+INSERT INTO `sys_menu` VALUES (44, '文件管理', '/file', 'el-icon-document', NULL, 4, 'File', 304);
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '描述',
+  `flag` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '唯一标识',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (1, '管理员', '管理员', 'ROLE_ADMIN');
+INSERT INTO `sys_role` VALUES (2, '普通用户', '普通用户', 'ROLE_NORMAL');
+INSERT INTO `sys_role` VALUES (3, '活动负责人', '活动负责人', 'ROLE_CHARGE');
+
+-- ----------------------------
+-- Table structure for sys_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu`  (
+  `role_id` int(0) NOT NULL COMMENT '角色id',
+  `menu_id` int(0) NOT NULL COMMENT '菜单id',
+  PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '角色菜单关系表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES (1, 1);
+INSERT INTO `sys_role_menu` VALUES (1, 2);
+INSERT INTO `sys_role_menu` VALUES (1, 3);
+INSERT INTO `sys_role_menu` VALUES (1, 4);
+INSERT INTO `sys_role_menu` VALUES (1, 5);
+INSERT INTO `sys_role_menu` VALUES (1, 6);
+INSERT INTO `sys_role_menu` VALUES (1, 7);
+INSERT INTO `sys_role_menu` VALUES (1, 41);
+INSERT INTO `sys_role_menu` VALUES (1, 42);
+INSERT INTO `sys_role_menu` VALUES (1, 43);
+INSERT INTO `sys_role_menu` VALUES (1, 44);
+INSERT INTO `sys_role_menu` VALUES (2, 1);
+INSERT INTO `sys_role_menu` VALUES (2, 2);
+INSERT INTO `sys_role_menu` VALUES (2, 3);
+INSERT INTO `sys_role_menu` VALUES (2, 5);
+INSERT INTO `sys_role_menu` VALUES (2, 7);
+INSERT INTO `sys_role_menu` VALUES (3, 1);
+INSERT INTO `sys_role_menu` VALUES (3, 2);
+INSERT INTO `sys_role_menu` VALUES (3, 3);
+INSERT INTO `sys_role_menu` VALUES (3, 5);
+INSERT INTO `sys_role_menu` VALUES (3, 6);
+INSERT INTO `sys_role_menu` VALUES (3, 7);
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '昵称',
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '电话',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '地址',
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `avatar_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '头像',
+  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '角色',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '贪肯奇', 'admin@qq.com', '13988997788', '广东深圳', '2023-05-06 14:57:16', 'http://localhost:9090/file/3f5ab4ac5cb4481e8bc6baacc2385313.jpeg', 'ROLE_ADMIN');
+INSERT INTO `sys_user` VALUES (2, 'user01', 'e10adc3949ba59abbe56e057f20f883e', '负责人', 'charge@qq.com', '13988997788', '湖北武汉', '2023-04-30 18:01:02', 'http://localhost:9090/file/3f5ab4ac5cb4481e8bc6baacc2385313.jpeg', 'ROLE_CHARGE');
+INSERT INTO `sys_user` VALUES (3, 'user02', 'e10adc3949ba59abbe56e057f20f883e', '普通用户', 'normal@qq.com', '13826907515', '湖北武汉', '2023-05-06 16:05:11', 'http://localhost:9090/file/3f5ab4ac5cb4481e8bc6baacc2385313.jpeg', 'ROLE_NORMAL');
+INSERT INTO `sys_user` VALUES (34, 'user03', 'e10adc3949ba59abbe56e057f20f883e', '普通用户', NULL, NULL, NULL, '2023-05-06 21:56:18', NULL, 'ROLE_NORMAL');
+
+-- ----------------------------
+-- Table structure for t_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `t_comment`;
+CREATE TABLE `t_comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '内容',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '评论人id',
+  `time` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '评论时间',
+  `pid` int(0) NULL DEFAULT NULL COMMENT '父id',
+  `origin_id` int(0) NULL DEFAULT NULL COMMENT '最上级评论id',
+  `article_id` int(0) NULL DEFAULT NULL COMMENT '关联文章的id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_comment
+-- ----------------------------
+INSERT INTO `t_comment` VALUES (1, '回复内容', 1, '2023-05-06 20:00:00', NULL, NULL, 1);
+INSERT INTO `t_comment` VALUES (2, '回复内容', 1, '2023-05-06 20:00:00', NULL, NULL, NULL);
+INSERT INTO `t_comment` VALUES (5, '回复内容', 1, '2023-05-06 20:00:00', NULL, NULL, 1);
+INSERT INTO `t_comment` VALUES (6, '回复内容', 1, '2023-05-06 20:00:00', 4, 4, 1);
+INSERT INTO `t_comment` VALUES (7, '回复内容', 1, '2023-05-06 20:00:00', 4, 4, 1);
+INSERT INTO `t_comment` VALUES (8, '回复内容', 1, '2023-05-06 20:00:00', 7, 7, 1);
+INSERT INTO `t_comment` VALUES (9, '回复内容', 1, '2023-05-06 20:00:00', 7, 7, 1);
+INSERT INTO `t_comment` VALUES (10, '回复内容', 1, '2023-05-06 20:00:00', 7, 4, 1);
+INSERT INTO `t_comment` VALUES (11, '回复内容', 16, '2023-05-06 20:00:00', 10, 4, 1);
+INSERT INTO `t_comment` VALUES (13, '回复内容', 28, '2023-05-06 20:00:00', 12, 12, 1);
+INSERT INTO `t_comment` VALUES (14, '回复内容', 20, '2023-05-06 20:00:00', 13, 12, 1);
+INSERT INTO `t_comment` VALUES (15, '回复内容', 20, '2023-05-06 20:00:00', 13, 12, 1);
+INSERT INTO `t_comment` VALUES (16, '回复内容', 20, '2023-05-06 20:00:00', 4, 4, 1);
+INSERT INTO `t_comment` VALUES (17, '回复内容', 20, '2023-05-06 20:00:00', 5, 5, 1);
+INSERT INTO `t_comment` VALUES (19, '回复内容', 20, '2023-05-06 20:00:00', 1, 1, 1);
+INSERT INTO `t_comment` VALUES (21, '回复内容', 28, '2022-03-22 21:50:04', 20, 1, 1);
+INSERT INTO `t_comment` VALUES (22, '好！！！', 3, '2023-05-06 16:47:53', NULL, NULL, 2);
+
+SET FOREIGN_KEY_CHECKS = 1;
